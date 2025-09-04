985083f3b41a2cfec0de019f5bd49c584745f6e9
xiajia666 <xj1101295740@163.com>
2025-09-04 21:31:33 +0800

    新增Vite域名

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0', // 监听所有地址
-    port: 5173       // 端口可改，默认5173
+    port: 5173,       // 端口可改，默认5173
+    allowedHosts: ['arraydance.xyz', 'www.arraydance.xyz']
   }
 })
